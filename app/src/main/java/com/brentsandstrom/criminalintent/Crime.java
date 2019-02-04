9db1a3a39953716219d7b8825b42a6932c5b151d
BrentSandstrom <bsandstrom@bandtel.com>
2019-02-03 20:33:49 -0500

    Initial commit

---
+++ app/src/main/java/com/brentsandstrom/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.brentsandstrom.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Brent on 2/3/2019.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
